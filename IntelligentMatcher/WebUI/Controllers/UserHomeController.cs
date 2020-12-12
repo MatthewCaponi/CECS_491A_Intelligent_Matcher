0579d767f9d30966ace4b4aa55d29bc73431bb23
HoxNorf <45671870+HoxNorf@users.noreply.github.com>
2020-12-12 01:10:58 -0800

    User Info Fix

--- IntelligentMatcher/WebUI/Controllers/UserHomeController.cs
+++ IntelligentMatcher/WebUI/Controllers/UserHomeController.cs
@@ -28,6 +28,11 @@ namespace WebUI.Controllers
             return View();
         }
 
+        public IActionResult UserInfo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
