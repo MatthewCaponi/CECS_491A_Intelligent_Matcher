3b24336fb7151e09bd6fcd3f22296d93bfcb80d7
Jakub Koziol <jakubkoziol99@gmail.com>
2020-12-11 20:19:05 -0800

    Update TextLogTarget.cs
    Testing Pushing

--- IntelligentMatcher/Logging/TextLogTarget.cs
+++ IntelligentMatcher/Logging/TextLogTarget.cs
@@ -7,14 +7,87 @@ namespace Logging
 {
     public class TextLogTarget : ILogTarget
     {
-        public void LogToTarget(string message)
+        public void LogToTarget(string message, EventName eventname)
         {
-            throw new NotImplementedException();
+            if(eventname == EventName.SecurityEvent)
+            {
+                WriteSecurityLog(message);
+            }
+
+            if (eventname == EventName.NetworkEvent)
+            {
+                WriteNetworkLog(message);
+            }
+            if (eventname == EventName.UserEvent)
+            {
+                WriteUserLog(message);
+            }
         }
 
-        private void WriteText(string message, TextWriter w)
+        //Writes to the security log folder
+        //Checks to see if a log file alrady exists for the day if so appends if not create a new one and append to it
+        private void WriteSecurityLog(string message)
         {
 
+            string securityLogPath = "logs/security/security-log- " + DateTime.Today + ".txt";
+            if (!File.Exists(securityLogPath)){
+
+                using (StreamWriter writer = File.CreateText(securityLogPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(securityLogPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+
+        }
+
+        //Writes to the network log folder
+        //Checks to see if a log file alrady exists for the day if so appends if not create a new one and append to it
+        private void WriteNetworkLog(string message)
+        {
+            string securityLogPath = "logs/network/network-log- " + DateTime.Today + ".txt";
+            if (!File.Exists(securityLogPath))
+            {
+
+                using (StreamWriter writer = File.CreateText(securityLogPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(securityLogPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+        }
+        //Writes to the user log folder
+        //Checks to see if a log file alrady exists for the day if so appends if not create a new one and append to it
+        private void WriteUserLog(string message)
+        {
+            string securityLogPath = "logs/user/user-log- " + DateTime.Today + ".txt";
+            if (!File.Exists(securityLogPath))
+            {
+
+                using (StreamWriter writer = File.CreateText(securityLogPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(securityLogPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
         }
     }
 }
