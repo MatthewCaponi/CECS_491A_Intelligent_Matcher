74fae16e9db0bde6367c0ff3a52f6b26e014a511
MatthewCaponi <55855368+MatthewCaponi@users.noreply.github.com>
2020-12-12 02:06:45 -0800

    Fix problem hopefully

---
+++ IntelligentMatcher/BusinessLayerUnitTests/TextLogTargetTests.cs
@@ -0,0 +1,245 @@
+using Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace BusinessLayerUnitTests
+{
+    [TestClass]
+    public class TextLogTargetTests
+    {
+        [TestInitialize()]
+       public void Init()
+        {
+
+          
+        }
+
+        [TestCleanup()]
+        public void CleanUp()
+        {
+
+      
+        }
+
+        
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.UserEvent)]
+        public void LogToTarget_WriteUserEventToTextLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+            ILogTarget logTarget = new TextLogTarget();
+
+            string fileName = eventName + (DateTime.Today.Date).ToString(@"yyyy-MM-dd") + ".txt";
+            string directory = "C:\\Users\\" + Environment.UserName + "\\logs\\" + eventName.ToString();
+            string logPath = Path.Combine(directory, fileName);
+
+
+
+            // Act
+            logTarget.LogToTarget(expectedMessage, eventName);
+
+
+            //Assert
+            string actualMessage = LogTargetHelper.ReadTestLog(logPath);
+            Assert.IsTrue(actualMessage == expectedMessage.ToString());
+
+        }
+
+
+
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.SecurityEvent)]
+        public void LogToTarget_WriteSecurityEventToTextLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+            ILogTarget logTarget = new TextLogTarget();
+
+            string fileName = eventName + (DateTime.Today.Date).ToString(@"yyyy-MM-dd") + ".txt";
+            string directory = "C:\\Users\\" + Environment.UserName + "\\logs\\" + eventName.ToString();
+            string logPath = Path.Combine(directory, fileName);
+
+
+
+            // Act
+            logTarget.LogToTarget(expectedMessage, eventName);
+
+
+            //Assert
+            string actualMessage = LogTargetHelper.ReadTestLog(logPath);
+            Assert.IsTrue(actualMessage == expectedMessage.ToString());
+
+        }
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.NetworkEvent)]
+        public void LogToTarget_WriteNetworkEventToTextLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+            ILogTarget logTarget = new TextLogTarget();
+
+            string fileName = eventName + (DateTime.Today.Date).ToString(@"yyyy-MM-dd") + ".txt";
+            string directory = "C:\\Users\\" + Environment.UserName + "\\logs\\" + eventName.ToString();
+            string logPath = Path.Combine(directory, fileName);
+
+
+
+            // Act
+            logTarget.LogToTarget(expectedMessage, eventName);
+
+
+            //Assert
+            string actualMessage = LogTargetHelper.ReadTestLog(logPath);
+            Assert.IsTrue(actualMessage == expectedMessage.ToString());
+
+        }
+
+
+
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.UserEvent)]
+        public void LogToTarget_WriteUserEventToJSONLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+            ILogTarget logTarget = new JsonLogTarget();
+
+            string fileName = eventName + (DateTime.Today.Date).ToString(@"yyyy-MM-dd") + ".txt";
+            string directory = "C:\\Users\\" + Environment.UserName + "\\logs\\" + eventName.ToString();
+            string logPath = Path.Combine(directory, fileName);
+
+
+
+            // Act
+            logTarget.LogToTarget(expectedMessage, eventName);
+
+
+            //Assert
+            string actualMessage = LogTargetHelper.ReadTestLog(logPath);
+            Assert.IsTrue(actualMessage == expectedMessage.ToString());
+
+        }
+
+
+
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.SecurityEvent)]
+        public void LogToTarget_WriteSecurityEventToJSONLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+            ILogTarget logTarget = new JsonLogTarget();
+
+            string fileName = eventName + (DateTime.Today.Date).ToString(@"yyyy-MM-dd") + ".txt";
+            string directory = "C:\\Users\\" + Environment.UserName + "\\logs\\" + eventName.ToString();
+            string logPath = Path.Combine(directory, fileName);
+
+
+
+            // Act
+            logTarget.LogToTarget(expectedMessage, eventName);
+
+
+            //Assert
+            string actualMessage = LogTargetHelper.ReadTestLog(logPath);
+            Assert.IsTrue(actualMessage == expectedMessage.ToString());
+
+        }
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.NetworkEvent)]
+        public void LogToTarget_WriteNetworkEventToJSONLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+            ILogTarget logTarget = new JsonLogTarget();
+
+            string fileName = eventName + (DateTime.Today.Date).ToString(@"yyyy-MM-dd") + ".txt";
+            string directory = "C:\\Users\\" + Environment.UserName + "\\logs\\" + eventName.ToString();
+            string logPath = Path.Combine(directory, fileName);
+
+
+
+            // Act
+            logTarget.LogToTarget(expectedMessage, eventName);
+
+
+            //Assert
+            string actualMessage = LogTargetHelper.ReadTestLog(logPath);
+            Assert.IsTrue(actualMessage == expectedMessage.ToString());
+
+        }
+
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.NetworkEvent)]
+        public void LogToTarget_WriteNetworkEventToConsoleLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+
+            var currentConsoleOut = Console.Out;
+
+            ConsoleLogTarget target = new ConsoleLogTarget();
+
+
+            using (var consoleOutput = new ConsoleOutputChecker())
+            {
+                target.LogToTarget(expectedMessage, eventName);
+                expectedMessage = "New " + eventName.ToString() + ": " + expectedMessage;
+                Assert.AreEqual(expectedMessage, consoleOutput.GetOuput());
+            }
+
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+
+
+        }
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.UserEvent)]
+        public void LogToTarget_WriteUserEventToConsoleLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+
+            var currentConsoleOut = Console.Out;
+
+            ConsoleLogTarget target = new ConsoleLogTarget();
+
+
+            using (var consoleOutput = new ConsoleOutputChecker())
+            {
+                target.LogToTarget(expectedMessage, eventName);
+                expectedMessage = "New " + eventName.ToString() + ": " + expectedMessage;
+                Assert.AreEqual(expectedMessage, consoleOutput.GetOuput());
+            }
+
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+
+
+        }
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.SecurityEvent)]
+        public void LogToTarget_WriteSecurityEventToConsoleLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            // Arrange
+
+            var currentConsoleOut = Console.Out;
+
+            ConsoleLogTarget target = new ConsoleLogTarget();
+
+
+            using (var consoleOutput = new ConsoleOutputChecker())
+            {
+                target.LogToTarget(expectedMessage, eventName);
+                expectedMessage = "New " + eventName.ToString() + ": " + expectedMessage;
+                Assert.AreEqual(expectedMessage, consoleOutput.GetOuput());
+            }
+
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+
+
+        }
+
+
+    }
+}
