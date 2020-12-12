ac1b0298ba1f3794eff7a30f7bf59f5bd3761eb5
MatthewCaponi <55855368+MatthewCaponi@users.noreply.github.com>
2020-12-12 07:59:05 -0800

    Update UserProfileRepositoryTests.cs

--- IntelligentMatcher/DataAccessUnitTestes/UserProfileRepositoryTests.cs
+++ IntelligentMatcher/DataAccessUnitTestes/UserProfileRepositoryTests.cs
@@ -12,26 +12,31 @@ namespace DataAccessUnitTestes
     [TestClass]
     public class UserProfileRepositoryTests
     {
-        [TestInitialize()]
-        public async Task Init()
-        {
-            var numTestRows = 10;
+        //[TestInitialize()]
+        //public async Task Init()
+        //{
+        //    var numTestRows = 10;
 
-            IDataGateway dataGateway = new DataGateway();
-            IConnectionStringData connectionString = new ConnectionStringData();
-            ITestRepo testRepo = new TestRepo(dataGateway, connectionString);
+        //    IDataGateway dataGateway = new DataGateway();
+        //    IConnectionStringData connectionString = new ConnectionStringData();
+        //    ITestRepo testRepo = new TestRepo(dataGateway, connectionString);
 
-            for (int i = 1; i <= numTestRows; ++i)
-            {
-                UserAccountModel userAccountModel = new UserAccountModel("TestUser" + i, "TestPass" + i, "TestEmail" + i);
-                await testRepo.InsertUserAccountTestRows(userAccountModel);
+        //    for (int i = 1; i <= numTestRows; ++i)
+        //    {
+        //        UserAccountModel userAccountModel = new UserAccountModel("TestUser" + i, "TestPass" + i, "TestEmail" + i);
+        //        await testRepo.InsertUserAccountTestRows(userAccountModel); 
 
-                UserProfileModel model = new UserProfileModel($"FirstName {i}", $"LastName {i}", DateTime.Now, DateTime.Today.Date, 
-                    UserProfileModel.AccountType.User, UserProfileModel.AccountStatus.Active, userAccountModel);
+        //        UserProfileModel model = new UserProfileModel($"FirstName {i}", $"LastName {i}", DateTime.Now, DateTime.Today.Date,
+        //             UserProfileModel.AccountType.User.ToString(), UserProfileModel.AccountStatus.Active.ToString(), userAccountModel);
+        //        await testRepo.InsertUserProfileTestRows(model);
 
-                await testRepo.InsertUserProfileTestRows(model);
-            }
-        }
+        //    }
+
+
+
+
+
+        //}
 
         [TestCleanup()]
         public async Task CleanUp()
@@ -40,23 +45,24 @@ namespace DataAccessUnitTestes
             IConnectionStringData connectionString = new ConnectionStringData();
             ITestRepo testRepo = new TestRepo(dataGateway, connectionString);
 
+            await testRepo.DeleteUserAccountTestRows();
             await testRepo.DeleteUserProfileTestRows();
         }
 
         [DataTestMethod]
-        [DataRow("John014", "532jfosho", "jfosho@gmail.com")]
-        [DataRow("Lisa32", "Lisababy73", "llawrence@gmail.com")]
-        [DataRow("superguy234", "ilovecats", "catlover@yahoo.com")]
-        [DataRow("seiwla", "12345", "sei@gmail.com")]
-        public async Task CreateUserProfile_UserProfileDoesNotExist_ReturnsCorrectId(string username, string password, string email, string firstName, string lastName, DateTime dateOfBirth, 
-            DateTime accountCreationDate, UserProfileModel.AccountType accountType, UserProfileModel.AccountStatus accountStatus, UserAccountModel userAccountModel)
+        [DataRow("John014", "532jfosho", "jfosho@gmail.com", "John", "Fosho", UserProfileModel.AccountType.User, UserProfileModel.AccountStatus.Banned)]
+        [DataRow("Lisa32", "Lisababy73", "llawrence@gmail.com", "Lisa", "Baby", UserProfileModel.AccountType.User, UserProfileModel.AccountStatus.Banned)]
+        [DataRow("superguy234", "ilovecats", "catlover@yahoo.com", "Super", "Guy", UserProfileModel.AccountType.User, UserProfileModel.AccountStatus.Banned)]
+        [DataRow("seiwla", "12345", "sei@gmail.com", "Sei", "wa", UserProfileModel.AccountType.User, UserProfileModel.AccountStatus.Banned)]
+        public async Task CreateUserProfile_UserProfileDoesNotExist_ReturnsCorrectId(string username, string password, string email, string firstName, string lastName, 
+        UserProfileModel.AccountType accountType, UserProfileModel.AccountStatus accountStatus)
         {
             //Arrange
-            UserAccountModel model = new UserAccountModel(username, password, email);
-            UserProfileModel profileModel = new UserProfileModel(firstName, lastName, dateOfBirth, accountCreationDate, accountType, accountStatus, userAccountModel);
+            UserAccountModel userAccountModel = new UserAccountModel(username, password, email);
+            UserProfileModel profileModel = new UserProfileModel(firstName, lastName, DateTime.Now, DateTime.Today, accountType.ToString(), accountStatus.ToString(), userAccountModel);
             IUserAccountRepository userAccount = new UserAccountRepository(new DataGateway(), new ConnectionStringData());
             IUserProfileRepository userProfile = new UserProfileRepository(new DataGateway(), new ConnectionStringData());
-            await userAccount.CreateUserAccount(model);
+            await userAccount.CreateUserAccount(userAccountModel);
 
             //Act
             await userProfile.CreateUserProfile(profileModel);
