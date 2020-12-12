1d6e1db0cd23e8b0d22911b823e6b8c5aec3da24
Jakub Koziol <jakubkoziol99@gmail.com>
2020-12-11 20:38:52 -0800

    Update JsonLogTarget.cs

--- IntelligentMatcher/Logging/JsonLogTarget.cs
+++ IntelligentMatcher/Logging/JsonLogTarget.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.IO;
 namespace Logging
 {
     public class JsonLogTarget : ILogTarget
     {
-        public void LogToTarget(string message)
+        public void LogToTarget(string message, EventName eventname)
         {
-            throw new NotImplementedException();
+            WriteLog(message, eventname);
+        }
+
+
+        private void WriteLog(string message, EventName eventname)
+        {
+
+            string logPath = "logs/" + eventname.ToString() + "/" + eventname.ToString() + "-log- " + DateTime.Today + ".json";
+            if (!File.Exists(logPath))
+            {
+
+                using (StreamWriter writer = File.CreateText(logPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(logPath))
+                {
+                    writer.WriteLine(message);
+                }
+            }
+
         }
     }
 }
