3787fb84d9705e6360bdbe6f59df7009f1a763c7
Jakub Koziol <jakubkoziol99@gmail.com>
2020-12-11 20:33:14 -0800

    Update TextLogTarget.cs

--- IntelligentMatcher/Logging/TextLogTarget.cs
+++ IntelligentMatcher/Logging/TextLogTarget.cs
@@ -11,35 +11,27 @@ namespace Logging
         {
             if(eventname == EventName.SecurityEvent)
             {
-                WriteSecurityLog(message);
+                WriteSecurityLog(message, eventname);
             }
 
-            if (eventname == EventName.NetworkEvent)
-            {
-                WriteNetworkLog(message);
-            }
-            if (eventname == EventName.UserEvent)
-            {
-                WriteUserLog(message);
-            }
         }
 
         //Writes to the security log folder
         //Checks to see if a log file alrady exists for the day if so appends if not create a new one and append to it
-        private void WriteSecurityLog(string message)
+        private void WriteSecurityLog(string message, , EventName eventname)
         {
 
-            string securityLogPath = "logs/security/security-log- " + DateTime.Today + ".txt";
-            if (!File.Exists(securityLogPath)){
+            string logPath = "logs/" + eventname.ToString() + "/" + eventname.ToString() + "-log- " + DateTime.Today + ".txt";
+            if (!File.Exists(logPath)){
 
-                using (StreamWriter writer = File.CreateText(securityLogPath))
+                using (StreamWriter writer = File.CreateText(logPath))
                 {
                     writer.WriteLine(message);
                 }
             }
             else
             {
-                using (StreamWriter writer = File.AppendText(securityLogPath))
+                using (StreamWriter writer = File.AppendText(logPath))
                 {
                     writer.WriteLine(message);
                 }
@@ -49,45 +41,9 @@ namespace Logging
 
         //Writes to the network log folder
         //Checks to see if a log file alrady exists for the day if so appends if not create a new one and append to it
-        private void WriteNetworkLog(string message)
-        {
-            string securityLogPath = "logs/network/network-log- " + DateTime.Today + ".txt";
-            if (!File.Exists(securityLogPath))
-            {
-
-                using (StreamWriter writer = File.CreateText(securityLogPath))
-                {
-                    writer.WriteLine(message);
-                }
-            }
-            else
-            {
-                using (StreamWriter writer = File.AppendText(securityLogPath))
-                {
-                    writer.WriteLine(message);
-                }
-            }
-        }
+   
         //Writes to the user log folder
         //Checks to see if a log file alrady exists for the day if so appends if not create a new one and append to it
-        private void WriteUserLog(string message)
-        {
-            string securityLogPath = "logs/user/user-log- " + DateTime.Today + ".txt";
-            if (!File.Exists(securityLogPath))
-            {
-
-                using (StreamWriter writer = File.CreateText(securityLogPath))
-                {
-                    writer.WriteLine(message);
-                }
-            }
-            else
-            {
-                using (StreamWriter writer = File.AppendText(securityLogPath))
-                {
-                    writer.WriteLine(message);
-                }
-            }
-        }
+ 
     }
 }
