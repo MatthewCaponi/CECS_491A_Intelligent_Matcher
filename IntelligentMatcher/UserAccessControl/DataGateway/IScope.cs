333eb17bfaaf3fad8499017eb7177d82cce8aa18
ian <47012459+ianxgrey@users.noreply.github.com>
2021-02-08 19:16:09 -0800

    Added IScope

---
+++ IntelligentMatcher/UserAccessControl/DataGateway/IScope.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserAccessControl
+{ 
+    public interface IScope {
+
+        void GetComponentsAccessible(List<string> componentsAccessible);
+
+        void GetActionsAccessible(List<string> actionsAccessible);
+
+        void SetComponentsAccessible(List<string> componentsAccessible);
+
+        void SetActionsAccessible(List<string> actionsAccessible);
+    }
+}
