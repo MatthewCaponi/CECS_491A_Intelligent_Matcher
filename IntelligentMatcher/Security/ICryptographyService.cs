4c336be4c62c2a6c88ed14dac3add6a5679e780e
Jakub Koziol <jakubkoziol99@gmail.com>
2021-02-25 12:04:44 -0800

    Cryptography Framework part 2
    Format isnt the best I will need to fix that I jsut wanted to get a framework going to Shaan will be able to use it for his Registration class.

--- IntelligentMatcher/Security/ICryptographyService.cs
+++ IntelligentMatcher/Security/ICryptographyService.cs
@@ -4,6 +4,36 @@ namespace Security
 {
     class ICryptographyService
     {
- 
+
+        private string createSalt(int UserID)
+        {
+            return "Salt";
+        }
+
+        private string retreiveSalt(int UserID)
+        {
+            return "Salt";
+        }
+        private string encrypt(string SaltedPassword)
+        {
+            byte[] Data = System.Text.Encoding.ASCII.GetBytes(SaltedPassword);
+            Data = new System.Security.Cryptography.SHA256Managed().ComputeHash(Data);
+            return (System.Text.Encoding.ASCII.GetString(Data).ToString());
+        }
+
+        public Boolean newPasswordEncrypt(string Password, int UserID)
+        {
+            string SaltedPassword = Password + createSalt(UserID);
+            string encryptedPassword = encrypt(SaltedPassword);
+            return (true);
+        }
+
+        public string encryptPassword(string Password, int UserID)
+        {
+            string SaltedPassword = Password + retreiveSalt(UserID);
+            string encryptedPassword = encrypt(SaltedPassword);
+            return (encryptedPassword);
+        }
+
     }
 }
