518788e7c1ad1f70a5c57a79bd74eab86bb0e8f9
Jakub Koziol <jakubkoziol99@gmail.com>
2021-02-25 11:48:20 -0800

    Cyrptography Framework

---
+++ IntelligentMatcher/BusinessLayerUnitTests/Security/CryptographyTests.cs
@@ -0,0 +1,36 @@
+﻿using Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+
+namespace BusinessLayerUnitTests.Security
+{
+    [TestClass]
+    public class CyrptographyTests
+    {
+        [TestInitialize()]
+        public void Init()
+        {
+
+
+        }
+
+        [TestCleanup()]
+        public void CleanUp()
+        {
+
+
+        }
+
+
+        [DataTestMethod]
+        [DataRow("TextLogTest", EventName.SecurityEvent)]
+        public void LogToTarget_WriteSecurityEventToConsoleLog_ReadTextSucccessful(string expectedMessage, EventName eventName)
+        {
+            Assert.IsTrue(true);
+        }
+
+
+    }
+}
