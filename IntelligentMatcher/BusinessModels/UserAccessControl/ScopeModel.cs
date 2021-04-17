7be78dcf79043979f00e4b472c51e2a87b42f370
MatthewCaponi <55855368+MatthewCaponi@users.noreply.github.com>
2021-04-17 16:17:33 -0700

    Added User Access Control Business Models

---
+++ IntelligentMatcher/BusinessModels/UserAccessControl/ScopeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessModels.UserAccessControl
+{
+    public class ScopeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Default { get; set; }
+        public List<ClaimModel> Claims { get; set; }
+    }
+}
