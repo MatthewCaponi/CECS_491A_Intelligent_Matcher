f434baaa6a9aab2ed6dc353269f389f2429093c5
HoxNorf <45671870+HoxNorf@users.noreply.github.com>
2021-05-02 00:02:07 -0700

    ArchiveService Start

---
+++ IntelligentMatcher/BusinessLayerUnitTests/Archiving/ArchiveServiceTests.cs
@@ -0,0 +1,88 @@
+﻿using Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.Archiving;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BusinessLayerUnitTests.Archiving
+{
+    [TestClass]
+    public class ArchiveServiceTests
+    {
+        [DataTestMethod]
+        [DataRow("TestLog1.txt", "TestLog2.txt", "TestLog3.txt")]
+        public void ArchiveService_ArchiveSuccess_ReturnTrue(string file1, string file2, string file3)
+        {
+            // Arrange
+            string currentDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(currentDirectory).FullName;
+            string textDirectory = $"{projectDirectory}\\logs";
+            string logPath1 = Path.Combine(textDirectory, file1);
+            string logPath2 = Path.Combine(textDirectory, file2);
+            string logPath3 = Path.Combine(textDirectory, file3);
+
+            string builtMessage = "Test Log";
+
+            if (!File.Exists(logPath1))
+            {
+                using (StreamWriter writer = File.CreateText(logPath1))
+                {
+                    writer.WriteLine(builtMessage);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(logPath1))
+                {
+                    writer.WriteLine(builtMessage);
+                }
+            }
+
+            if (!File.Exists(logPath2))
+            {
+                using (StreamWriter writer = File.CreateText(logPath2))
+                {
+                    writer.WriteLine(builtMessage);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(logPath2))
+                {
+                    writer.WriteLine(builtMessage);
+                }
+            }
+
+            if (!File.Exists(logPath3))
+            {
+                using (StreamWriter writer = File.CreateText(logPath3))
+                {
+                    writer.WriteLine(builtMessage);
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = File.AppendText(logPath3))
+                {
+                    writer.WriteLine(builtMessage);
+                }
+            }
+
+            List<string> files = new List<string>();
+
+            files.Add(logPath1);
+            files.Add(logPath2);
+            files.Add(logPath3);
+
+            IArchiveService archiveService = new ArchiveService();
+
+            // Act
+            var result = archiveService.ArchiveLogFiles(files);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ IntelligentMatcher/Services/Archiving/ArchiveService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Archiving
+{
+    public class ArchiveService : IArchiveService
+    {
+        public bool ArchiveLogFiles(List<string> files)
+        {
+            try
+            {
+                string currentDirectory = Environment.CurrentDirectory;
+                string projectDirectory = Directory.GetParent(currentDirectory).FullName;
+                string targetPath = $"{projectDirectory}\\archived";
+
+                if (files.Count <= 0)
+                {
+                    return false;
+                }
+
+                //create the archive directory if it does not exist
+                if (!Directory.Exists(targetPath))
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(targetPath);
+                }
+
+                foreach (string file in files)
+                {
+                    var fileName = Path.GetFileName(file);
+                    string targetFile = Path.Combine(targetPath, fileName);
+                    if (File.Exists(targetFile))
+                    {
+                        File.Delete(targetFile);
+                    }
+                    File.Move(file, targetFile);
+                    //File.Delete(file);
+                }
+
+                return true;
+            }
+            catch (IOException e)
+            {
+                throw new IOException(e.Message, e.InnerException);
+            }
+        }
+    }
+}
