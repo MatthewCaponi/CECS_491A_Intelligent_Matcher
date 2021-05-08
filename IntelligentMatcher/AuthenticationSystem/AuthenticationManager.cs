203d9ab4c05d767f0d0b7fb5d7776ef624f8eb57
MatthewCaponi <55855368+MatthewCaponi@users.noreply.github.com>
2021-05-07 22:49:49 -0700

    Authenticate Stuff

--- IntelligentMatcher/AuthenticationSystem/AuthenticationManager.cs
+++ IntelligentMatcher/AuthenticationSystem/AuthenticationManager.cs
@@ -6,17 +6,17 @@ namespace AuthenticationSystem
 {
     public class AuthenticationManager
     {
-        //private readonly ITokenService _tokenService;
+        private readonly ITokenService _tokenService;
 
-        //public AuthenticationManager(ITokenService tokenService)
-        //{
-        //    _tokenService = tokenService;
-        //}
+        public AuthenticationManager(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
 
-        //public bool Authenticate(string token, AssignmentPolicyModel assignmentPolicyModel)
-        //{
-        //    var claims = _tokenService.ExtractClaims(token);
+        public bool Authenticate(string token, AssignmentPolicyModel assignmentPolicyModel)
+        {
+            var claims = _tokenService.ExtractClaims(token);
 
-        //}
+        }
     }
 }
