976912ab91ef8475129ffac79a80872bfdd99301
Jakub Koziol <jakubkoziol99@gmail.com>
2021-05-08 02:27:02 -0700

    controller

---
+++ IntelligentMatcher/WebApi/Controllers/UserAnalysisController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserAnalysisManager;
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class UserAnalysisController : Controller
+    {
+
+        private readonly IUserAnalysisService _userAnalysisService;
+
+        public UserAnalysisController(IUserAnalysisService userAnalysisService)
+        {
+            _userAnalysisService = userAnalysisService;
+        }
+        [HttpPost]
+        public async Task<ActionResult<int>> GetRegistrationCount()
+        {
+            try
+            {
+                return Ok(await _userAnalysisService.GetALLUserRegistrationCount());
+
+            }
+            catch
+            {
+                return StatusCode(404);
+
+            }
+        }
+    }
+}
